bcbd05c7b3c6a4abd38adf55cb29092759790bdf
skale <dev@skale.dev>
2025-10-15 13:43:14 +0200

    init

---
+++ agentos/agentos.py
@@ -0,0 +1,30 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.os import AgentOS
+from credgoo import get_api_key
+
+api_key = get_api_key("tu")
+
+
+assistant = Agent(
+    name="Assistant",
+    model=OpenAIChat(
+        id="qwen-coder-30b",
+        base_url="https://aqueduct.ai.datalab.tuwien.ac.at/v1",
+        api_key=api_key,
+    ),
+    instructions=["You are a helpful AI assistant."],
+    markdown=True,
+)
+
+agent_os = AgentOS(
+    id="glmos",
+    description="glm agentos",
+    agents=[assistant],
+)
+
+app = agent_os.get_app()
+
+if __name__ == "__main__":
+    # Default port is 7777; change with port=...
+    agent_os.serve(app="agentos:app", reload=True)
